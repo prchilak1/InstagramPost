e40bed6bc8d7b84ff3ecf9073b0dca472d359158
prchilak1 <pratyusha@zyxan.tech>
2024-06-18 22:43:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='content-wrapper'>
+        <div className='image-wrapper'>
+          <img
+            className='post'
+            src={require('./assets/Screenshot1.png')}
+            alt='no-image1'
+          />
+          <div className='next-icon'>
+            <img
+              style={{ width: '100%', height: '100%' }}
+              src={require('./assets/Screenshot6.png')}
+              alt='no-image2'
+            />
+          </div>
+        </div>
+        <div className='footer'>
+          <div className='left'>
+            <img
+              className='like-icon'
+              src={require('./assets/Screenshot2.png')}
+              alt='no-image1'
+            />
+            <img
+              className='message-icon'
+              src={require('./assets/Screenshot3.png')}
+              alt='no-image1'
+            />
+            <img
+              className='share-icon'
+              src={require('./assets/Screenshot4.png')}
+              alt='no-image1'
+            />
+          </div>
+          <img
+            className='save-icon'
+            src={require('./assets/Screenshot5.png')}
+            alt='no-image1'
+          />
+        </div>
+      </div>
     </div>
   );
 }
